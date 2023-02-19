f920c31f4774dc2cc71f03b619e62a1d033f9304
luoj976 <luo.jing@northeastern.edu>
2023-02-19 01:26:39 -0800

    update

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
 from mybot import ask, append_interaction_to_chat_log
+from twilio.rest import Client
+import os
+
 
 app = Flask(__name__)
 # if for some reason your conversation with the bot gets weird, change the secret key 
 app.config['SECRET_KEY'] = '89djhff9lhkd93'
 
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
+
 @app.route('/mybot', methods=['POST'])
 def jabe():
-    incoming_msg = request.values['Body']
+    incoming_msg = request.form['Body']
+    sender_id = request.form['From']
     chat_log = session.get('chat_log')
     answer = ask(incoming_msg, chat_log)
     session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
                                                          chat_log)
-    msg = MessagingResponse()
-    msg.message(answer)
-    return str(msg)
+    if answer['status'] == 1:
+            
+        msg = answer['response'];
+        client.messages.create(
+            from_=os.getenv('FROM'),
+            body= msg,
+            to= sender_id,
+        )
+        return str(msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
