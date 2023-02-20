99bdafeea5792ba69ba3fd132c9a0f79664a129d
luoj976 <luo.jing@northeastern.edu>
2023-02-19 22:44:36 -0800

    12

--- app.py
+++ app.py
@@ -1,43 +1,3 @@
-# from flask import Flask, request, session
-# from twilio.twiml.messaging_response import MessagingResponse
-# # from mybot import ask, append_interaction_to_chat_log
-# from twilio.rest import Client
-# import os
-
-
-# app = Flask(__name__)
-# # if for some reason your conversation with the bot gets weird, change the secret key 
-# app.config['SECRET_KEY'] = '89djhff9lhkd93'
-
-# account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-# auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-
-
-# @app.route('/')
-# def home():
-#     return 'All is well...'
-
-# @app.route('/mybot', methods=['POST'])
-# async def mybot():
-#     try:
-#         question = request.form['Body']
-#         sender_id = request.form['From']
-        
-#         answer = ask(question, None)
-#         # session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
-#                                                       # chat_log)
-#         if answer['status'] == 1:
-#             client = Client(account_sid, auth_token)
-#             client.messages.create(
-#                 from_=os.getenv('FROM'),
-#                 body=answer['response'],
-#                 to=sender_id,
-#             )
-        
-#     except:
-#         pass
-#     return "OK",200
-
 
 
 from openai_api import text_complition
@@ -57,7 +17,7 @@ def home():
     return 'All is well...'
 
 
-@app.route('/mybot', methods=['POST'])
+@app.route('/twilio/receiveMessage', methods=['POST'])
 def receiveMessage():
     try:
         # Extract incomng parameters from Twilio
