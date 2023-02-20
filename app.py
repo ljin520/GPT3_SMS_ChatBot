4736267a8f2ce225de30bd3ea24bb55b105ebcfc
luoj976 <luo.jing@northeastern.edu>
2023-02-19 21:26:11 -0800

    update

--- app.py
+++ app.py
@@ -11,25 +11,22 @@ app.config['SECRET_KEY'] = '89djhff9lhkd93'
 
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-client = Client(account_sid, auth_token)
+
 
 @app.route('/mybot', methods=['POST'])
-def mybot():
-    incoming_msg = request.form['Body']
-    sender_id = request.form['From']
-    chat_log = session.get('chat_log')
-    answer = ask(incoming_msg, chat_log)
-    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
-                                                         chat_log)
-    if answer['status'] == 1:
-            
-        msg = answer['response'];
-        client.messages.create(
-            from_=os.getenv('FROM'),
-            body= msg,
-            to= sender_id,
-        )
+async def mybot():
+    try:
+        incoming_msg = request.values['Body']
+        chat_log = session.get('chat_log')
+        answer = ask(incoming_msg, chat_log)
+        session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                            chat_log)
+        msg = MessagingResponse()
+        msg.message(answer)
         return str(msg)
+    except:
+        pass
+        return "Err"
 
 if __name__ == '__main__':
     app.run(debug=True)
