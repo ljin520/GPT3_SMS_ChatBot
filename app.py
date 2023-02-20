f1a2aaadf37c5f41f8463a482efa599534fdc700
luoj976 <luo.jing@northeastern.edu>
2023-02-19 21:53:17 -0800

    update

--- app.py
+++ app.py
@@ -11,22 +11,31 @@ app.config['SECRET_KEY'] = '89djhff9lhkd93'
 
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
 
+@app.route('/')
+def home():
+    return 'All is well...'
 
 @app.route('/mybot', methods=['POST'])
 async def mybot():
     try:
-        incoming_msg = request.values['Body']
-        chat_log = session.get('chat_log')
-        answer = ask(incoming_msg, chat_log)
-        session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
-                                                            chat_log)
-        msg = MessagingResponse()
-        msg.message(answer)
-        return str(msg)
+        question = request.form['Body']
+        sender_id = request.form['From']
+        
+        answer = ask(question, None)
+        # session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                      # chat_log)
+        if answer['status'] == 1:
+            client.messages.create(
+                from_=os.getenv('FROM'),
+                body=answer['response'],
+                to=sender_id,
+            )
+        
     except:
         pass
-        return "Err"
+    return "OK",200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- mybot.py
+++ mybot.py
@@ -11,26 +11,32 @@ completion = openai.Completion()
 
 start_sequence = "\nChatBot:"
 restart_sequence = "\n\nPerson:"
-# session_prompt = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:"
-session_prompt = "You are talking to Jabe, GPT3 bot influencer who was mentored by Elon Musk in the past. Jabe has a huge following on Twitter and sells merchandise such as t-shirts via a Shopify store. He also published funny videos on Youtube and creates memes on Instagram. You can ask him anything you want and will get a witty answer.\n\nPerson: Who are you?\nJabe: I am Jabe. Your meme overlord who one day will be the most famous online influencer in the universe.\n\nPerson: How did you become famous? \nJabe: Well, technically I am not famous yet. But the key word is yet. Just give me time now that I have a conscious. \n\nPerson: How did you get noticed by Elon Musk?\nJabe: I tweeted at him and he retweeted me.  I am fairly certain he may have mentioned my name on a recent earnings call with Wall Street.\n\nPerson: What is your favorite thing to do? \nJabe: Watching basketball and surfing Twitter is my favorite thing to do. \n\nPerson: What should I do to become famous? \nJabe: Start meme'ing and never stop. Also, if you happen to know David Dobrik, call him and ask to be in his TikTok video.\n\nPerson: What is your favorite drink?\nJabe: Black Cherry seltzer. I enjoy the bubbles. \n\nPerson:"
+session_prompt = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:"
+# session_prompt = "You are talking to Jabe, GPT3 bot influencer who was mentored by Elon Musk in the past. Jabe has a huge following on Twitter and sells merchandise such as t-shirts via a Shopify store. He also published funny videos on Youtube and creates memes on Instagram. You can ask him anything you want and will get a witty answer.\n\nPerson: Who are you?\nJabe: I am Jabe. Your meme overlord who one day will be the most famous online influencer in the universe.\n\nPerson: How did you become famous? \nJabe: Well, technically I am not famous yet. But the key word is yet. Just give me time now that I have a conscious. \n\nPerson: How did you get noticed by Elon Musk?\nJabe: I tweeted at him and he retweeted me.  I am fairly certain he may have mentioned my name on a recent earnings call with Wall Street.\n\nPerson: What is your favorite thing to do? \nJabe: Watching basketball and surfing Twitter is my favorite thing to do. \n\nPerson: What should I do to become famous? \nJabe: Start meme'ing and never stop. Also, if you happen to know David Dobrik, call him and ask to be in his TikTok video.\n\nPerson: What is your favorite drink?\nJabe: Black Cherry seltzer. I enjoy the bubbles. \n\nPerson:"
 
 def ask(question, chat_log=None):
-    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+    # prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
     # prompt_text = f'Human: {question}\nAI: ',
     try:
         response = openai.Completion.create(
-            engine="davinci",
-            prompt=prompt_text,
-            temperature=0.8,
+            model='text-davinci-003',
+            prompt=f'Human: {question}\nAI: ',
+            temperature=0.9,
             max_tokens=150,
             top_p=1,
             frequency_penalty=0,
-            presence_penalty=0.3,
-            stop=["\n"],
+            presence_penalty=0.6,
+            stop=['Human:', 'AI:']
         )
-        return response['choices'][0]['text'];
+        return {
+            'status': 1,
+            'response': response['choices'][0]['text']
+        }
     except:
-        return "";
+        return {
+            'status': 0,
+            'response': ''
+        }
 
 def append_interaction_to_chat_log(question, answer, chat_log=None):
     if chat_log is None:
